13203f781284641e15532ee8e9f1d35bce0b2637
emptymonkey <pseudorandom@gmail.com>
2024-07-18 20:37:00 +0000

    paws moved to it's own project

---
+++ paws/paws-minimal.c
@@ -0,0 +1,23 @@
+
+/* A minimal version of paws */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void){
+
+
+	while(1){
+
+		// Be extra kawaii!!
+		printf("\nKawaii Bear says:\n");
+		printf("\ti hav paws!\n\t");
+		printf("ฅʕ·ᴥ·ʔฅ");
+		printf("\n\n");
+
+		// pause() to allow the human to inspect the process.
+		pause();
+	}
+
+	return(0);
+}
